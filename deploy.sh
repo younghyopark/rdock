1bd6b15d428b3112fc063fe0b916159c652a7c7d
Younghyo Park <younghyo@mit.edu>
2025-12-18 19:12:18 -0500

    Fix: add missing print_info function to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,11 @@ set -e
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 print_status() { echo -e "${GREEN}[✓]${NC} $1"; }
+print_info() { echo -e "${BLUE}[i]${NC} $1"; }
 print_warning() { echo -e "${YELLOW}[!]${NC} $1"; }
 print_error() { echo -e "${RED}[✗]${NC} $1"; }
 
